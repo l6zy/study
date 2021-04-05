ca3a18d0b9990ca9551418f6c39dce33b2608337
l6zy <67750535+l6zy@users.noreply.github.com>
2021-04-05 16:21:06 +0800

    Update IPValidity.cpp

--- Algorithm/IPValidity.cpp
+++ Algorithm/IPValidity.cpp
@@ -1,101 +1,110 @@
+// IPValidity.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
 #include <iostream>
+#define MAX 12
 using namespace std;
 
-int GetPossibleIp(int a[], int total)
+int str2int(char ip[], int start, int end)
 {
-    //��¼ÿһ��λ��
-    int n[4] = {1, 1, 1, 1};
-    int count = 0;
-    for (int i = 1; i < 4; i++)
-    {
-        n[0] = i;
-        for (int j = 1; j < 4; j++)
-        {
-            n[1] = j;
-            for (int k = 1; k < 4; k++)
-            {
-                n[2] = k;
-                for (int l = 1; l < 4; l++)
-                {
-                    n[3] = l;
-                    if (i + j + k + l == total)
-                    {
-                        //�ж��Ƿ���λ����Ϊ0
-                        if ((a[0] == 0 && i > 1) || (a[i] == 0 && j > 1) || (a[i + j] == 0 && k > 1) || (a[i + j + k] == 0 && l > 1))
-                        {
-                            continue;
-                        }
+	int digit, sum = 0;
+	for (int i = start; i <= end; i++)
+	{
+		digit = ip[i] - '0';
+		sum = sum * 10 + digit;
+	}
+	return sum;
+}
 
-                        //�ж�ÿһ���Ƿ񳬹�255
-                        if (n[0] == 3)
-                        {
-                            if (a[0] * 100 + a[1] * 10 + a[2] > 255)
-                            {
-                                continue;
-                            }
-                        }
+int IsIP(char ip[], int start, int end)
+{
+	int temp = str2int(ip, start, end);
+	if (temp > 255 || (start != end && ip[start] == '0'))
+	{
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
 
-                        if (n[1] == 3)
-                        {
-                            if (a[0 + i] * 100 + a[1 + i] * 10 + a[2 + i] > 255)
-                            {
-                                continue;
-                            }
-                        }
+//穷举
+int QJIP(char ip[],int n)
+{
+	int count = 0;
+	for (int i = 1; i <= 3; i++)
+	{
+		if (i > n || !IsIP(ip, 0, i - 1))
+			break;
+		for (int j = 1; j <= 3; j++)
+		{
+			if (i + j > n || !IsIP(ip, i, i + j - 1))
+				break;
+			for (int k = 1; k <= 3; k++)
+			{
+				if (i + j + k > n || !IsIP(ip, i + j, i + j + k - 1))
+					break;
+				for (int l = 1; l <= 3; l++)
+				{
+					if (i + j + k + l == n && IsIP(ip,i + j + k,i + j + k + l - 1))
+					{
+						count++;
+						cout <<str2int(ip, 0, i - 1) << "." << str2int(ip, i, i + j - 1) << "." << str2int(ip, i + j, i + j + k - 1) << "." << str2int(ip, i + j + k, n - 1) << endl;
+					}
+				}
+			}
+		}
+	}
+	return count;
+}
 
-                        if (n[2] == 3)
-                        {
-                            if (a[0 + i + j] * 100 + a[1 + i + j] * 10 + a[2 + i + j] > 255)
-                            {
-                                continue;
-                            }
-                        }
-
-                        if (n[3] == 3)
-                        {
-                            if (a[0 + i + j + k] * 100 + a[1 + i + j + k] * 10 + a[2 + i + j + k] > 255)
-                            {
-                                continue;
-                            }
-                        }
-
-                        //����Ϸ�IP
-                        int b = 0;
-                        while (b != total)
-                        {
-                            cout << a[b];
-                            b++;
-                            if (b == n[0] || b == n[0] + n[1] || b == n[0] + n[1] + n[2])
-                            {
-                                cout << ".";
-                            }
-                        }
-                        cout << "\n";
-
-                        count++;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-            }
-        }
-    }
-    return count;
+//动态规划
+int DPIP(char ip[], int n)
+{
+	int sum[12][4] = {};
+	for (int i = 0; i < 4; i++)
+	{
+		sum[i][i] = 1;
+	}
+	if (ip[0] != '0')
+	{
+		sum[1][0] = 1;
+		if (IsIP(ip, 0, 2))
+			sum[2][0] = 1;
+	}
+	for (int i = 2; i < n; i++)
+	{
+		for (int j = 1; j < 4; j++)
+		{
+			if (i > j)
+			{
+				if (i - 1 >= 0)
+				{
+					sum[i][j] += sum[i - 1][j - 1];
+					if (i - 2 >= 0 && IsIP(ip,i - 1,i))
+					{
+						sum[i][j] += sum[i - 2][j - 1];
+						if (i - 3 >= 0 && IsIP(ip,i - 2,i))
+						{
+							sum[i][j] += sum[i - 3][j - 1];
+						}
+					}
+				}
+			}
+		}
+	}
+	return sum[n - 1][3];
 }
 
 int main()
 {
-    int a[12];
-    int i = 0;
-    cin >> a[i];
-    i++;
-    do
-    {
-        cin >> a[i];
-        i++;
-    } while (getchar() != '\n');
-    cout << "---------------------------" << endl;
-    cout << GetPossibleIp(a, i) << " �ֿ���";
+	char ip[MAX];
+	cout << "请输入字符串：";
+	cin >> ip;
+	int n = strlen(ip);
+	cout << "----------------------" << endl;
+	int all = QJIP(ip, n);
+	cout << "----------------------" << endl;
+	cout << "穷  举  法：" << all << " 种情况" << endl;
+	cout << "动态规划法：" << DPIP(ip,n) << " 种情况";
 }
